356fcf211c1f8324364a57e70645fa5a30672d7f
Matt Sy <matthew.sy4@gmail.com>
2021-01-18 20:54:43 -0800

    updated DELETE call to return user object

--- sample_backend.py
+++ sample_backend.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 from flask_cors import CORS
+from random_username.generate import generate_username
 app = Flask(__name__)
 CORS(app) # <--- add this line
 
@@ -53,20 +54,23 @@ def get_users():
       return users
    elif request.method == 'POST':
       userToAdd = request.get_json()
+      userToAdd['id'] = generate_username(1)[0]
       users['users_list'].append(userToAdd)
       resp = jsonify(success=True)
-      #resp.status_code = 200 #optionally, you can always set a response code. 
+      resp.status_code = 201 #optionally, you can always set a response code. 
       # 200 is the default code for a normal response
-      return resp
+      return userToAdd
 
-@app.route('/users/<id>', methods=['DELETE'])
+@app.route('/users/<id>', methods=['DELETE', 'GET'])
 def get_user(id):
    if id :
       if request.method == 'DELETE':
          for user in users['users_list']:
             if user['id'] == id:
                users['users_list'].remove(user)
-      else:
+               return user, 204
+         return jsonify(message = "No user found with that index"), 404
+      elif request.method == 'GET':
          for user in users['users_list']:
             if user['id'] == id:
                return user
